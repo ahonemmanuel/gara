7be02304e290b6e804299993d22383baff99dd12
Emmanuel Kodjo <emmanuelahonsu@gmail.com>
2025-10-11 07:58:31 +0000

    all

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         User::create([
             'name' => 'Admin',
-            'email' => 'adminn@gmail.com',
+            'email' => 'admin@gmail.com',
             'password' => Hash::make('password'),
             'role' => 'admin',
             'approved' => true,
